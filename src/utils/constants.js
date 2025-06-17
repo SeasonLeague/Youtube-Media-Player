9ffcc69b7dddd0948daff6849b42ed336b2f6236
Godspower Maurice <111581268+SeasonLeague@users.noreply.github.com>
2025-06-17 22:29:07 +0100

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import HomeIcon from '@mui/icons-material/Home';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+import SchoolIcon from '@mui/icons-material/School';
+import ScienceIcon from '@mui/icons-material/Science';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import MemoryIcon from '@mui/icons-material/Memory';
+import CodeIcon from '@mui/icons-material/Code';
+import CloudIcon from '@mui/icons-material/Cloud';
+import SecurityIcon from '@mui/icons-material/Security';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+export const logo = 'https://i.postimg.cc/kX7jVSH4/Screenshot-2024-01-01-155943-removebg-preview-1.png';
+
+export const categories = [
+  { name: 'Machine Learning', icon: <PsychologyIcon />, },
+  { name: 'Deep Learning', icon: <MemoryIcon />, },
+  { name: 'Data Science', icon: <AnalyticsIcon />, },
+  { name: 'Python', icon: <CodeIcon />, },
+  { name: 'TensorFlow', icon: <AutoGraphIcon />, },
+  { name: 'PyTorch', icon: <ScienceIcon />, },
+  { name: 'AI', icon: <SmartToyIcon /> },
+  { name: 'Neural Networks', icon: <DataObjectIcon />, },
+  { name: 'Statistics', icon: <FunctionsIcon />, },
+  { name: 'Data Analysis', icon: <TrendingUpIcon />, },
+  { name: 'Computer Vision', icon: <VisibilityIcon />, },
+  { name: 'NLP', icon: <PsychologyIcon />, },
+  { name: 'MLOps', icon: <CloudIcon />, },
+  { name: 'Algorithms', icon: <FunctionsIcon />, },
+  { name: 'Data Mining', icon: <SecurityIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
